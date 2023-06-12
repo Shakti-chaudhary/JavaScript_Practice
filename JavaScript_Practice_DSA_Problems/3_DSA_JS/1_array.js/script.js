00cbc2315106a1aac742e073121ebc2bf1e6877d
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-12 18:13:20 +0530

    Day 3

---
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/1_array.js/script.js
@@ -0,0 +1,10 @@
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+// Omega {Best case}: Ω(1)
+// Theta {Average case}: θ(n)
+// Omicron {Worst case}: O(n)
+
+// Big O Drop Constants  {O(2N) -> O(N)}
+
+// Big O(N^2) {O(N^3) -> O(N^2)}
