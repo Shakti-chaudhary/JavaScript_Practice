37069ee9a269c0c14de85469ab415a547a33a7ad
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-13 18:11:43 +0530

    Day 4

--- JavaScript_Practice_DSA_Problems/3_DSA_JS/3_linkedList/1_LL.js
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/3_linkedList/1_LL.js
@@ -5,3 +5,36 @@
 // Big O(n) - Deletion from last and middle
 // Big O(1) - Deletion from front
 // Big O(n) - Search
+
+//  ========= LinkedList ===========
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor(data) {
+    const newNode = new Node(data);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+  push(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length++;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+let newLinkedList = new LinkedList(1);
+newLinkedList.push(2);
+console.log(newLinkedList);
