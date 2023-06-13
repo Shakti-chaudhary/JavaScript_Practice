e4d078a935fb2eef5841fabb1d04bcc23953519f
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-13 17:19:13 +0530

    Day 4

---
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/3_linkedList/1_LL.js
@@ -0,0 +1,7 @@
+// ========= LL BIG O ==================
+
+// Big O(n) - Insertion to last and middle
+// Big O(1) - Insertion to front
+// Big O(n) - Deletion from last and middle
+// Big O(1) - Deletion from front
+// Big O(n) - Search
