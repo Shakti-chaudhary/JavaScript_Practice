28115f8c2b1628815e2aeb91905b962ed261cfde
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-13 15:56:07 +0530

    Day 4

--- JavaScript_Practice_DSA_Problems/3_DSA_JS/1_array.js/script.js
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/1_array.js/script.js
@@ -5,6 +5,34 @@
 // Theta {Average case}: θ(n)
 // Omicron {Worst case}: O(n)
 
-// Big O Drop Constants  {O(2N) -> O(N)}
+// ===== Big O(N) Drop Constants  {O(2N) -> O(N)} =======
 
-// Big O(N^2) {O(N^3) -> O(N^2)}
+// Big O(N^2) {O(N^3) -> O(N^2)} // Alwin squared
+
+// Big O{A+B} {O(A+B)} // A and B were input
+// Big O{A*B} {O(A*B)} // A and B were input and loop running nested
+
+// ============ Drop Non-Dominants ======================
+
+// Big O(N^2)  ----> {O(N^2 + N) }
+
+// ======================================================
+
+// Big O(1) // Also known as Constant
+
+//  Big O(logN)
+
+// =============== Big O in arrays ======================
+
+// Big O(1) // For push, pop
+
+// Big O(N) // For shift, unshift
+
+// Big O(N) // For splice
+
+// ========= N = 100 ===================================
+
+// Big O(1) = 1 {Constant}
+// Big O(logN) = 7 {Divide and Conquer}
+// Big O(N) = 100 {Proportional}
+// Big O(N^2) = 10000 {Loop within a loop}
