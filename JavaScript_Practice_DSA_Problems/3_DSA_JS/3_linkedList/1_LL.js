33aec6694b21e695087d26567d022689edd6e55c
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-14 11:16:21 +0530

    Day 5

--- JavaScript_Practice_DSA_Problems/3_DSA_JS/3_linkedList/1_LL.js
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/3_linkedList/1_LL.js
@@ -21,6 +21,7 @@ class LinkedList {
     this.tail = newNode;
     this.length = 1;
   }
+  // ============ push method =====================
   push(data) {
     const newNode = new Node(data);
     if (!this.head) {
@@ -34,7 +35,38 @@ class LinkedList {
     this.length++;
     return this;
   }
+  // ============ pop method ======================
+  pop() {
+    if (!this.head) return undefined;
+
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
 }
 let newLinkedList = new LinkedList(1);
 newLinkedList.push(2);
+console.log(newLinkedList);
+console.log(newLinkedList.length);
+console.log(newLinkedList.pop());
+console.log(newLinkedList);
+console.log(newLinkedList.length);
+newLinkedList.pop();
+console.log(newLinkedList);
+console.log(newLinkedList.pop());
 console.log(newLinkedList);
