46bea92deb1e871ac4b96d1d57c21182ab340f57
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-17 17:19:31 +0530

    Day 8

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/file4_functions.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/file4_functions.js
@@ -38,3 +38,28 @@ const printLog = function () {
 };
 
 printLog();
+
+// ============= Arrow function ==================
+
+const printLog2 = () => {
+  console.log("This is arrow function.");
+};
+
+printLog2();
+
+const isEven = (num) => num % 2 === 0;
+
+console.log(isEven(4));
+console.log(isEven(1));
+
+// =================== Hoisting =====================
+
+// We can call a function before it is defined..
+// It worked in function declaration case
+// but not in function expression and arrow function case
+
+hello();
+
+function hello() {
+  console.log("Hello");
+}
