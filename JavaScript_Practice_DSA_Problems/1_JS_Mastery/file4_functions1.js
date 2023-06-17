5be9a8be62a2b58158ea9f47d14afc749840a30f
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-17 23:10:13 +0530

    Day 8

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/file4_functions1.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/file4_functions1.js
@@ -27,3 +27,19 @@ function myApp() {
   console.log(firstName); // can't access firstName outside of function
 }
 myApp();
+
+// =========== Default parameters ===========
+
+function addTwo(a, b = -1) {
+  return a + b;
+}
+const ans = addTwo(5);
+console.log(ans);
+
+// =========== Rest parameters ===========
+
+function addThree(...numbers) {
+  return numbers.reduce((a, b) => a + b);
+}
+console.log(addThree(1, 2, 3, 4, 5));
+console.log(addThree(1));
