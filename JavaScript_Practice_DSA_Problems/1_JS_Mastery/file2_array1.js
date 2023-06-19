a96bab5ee4f22dc6fa0eae05b8b0d3960183bd84
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-19 18:13:50 +0530

    Day 10

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/file2_array1.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/file2_array1.js
@@ -58,3 +58,24 @@ console.log(every);
 
 let some = arr2.some((value) => value % 2 === 0);
 console.log(some);
+
+// Fill methods
+// value , start , end
+// change main array
+
+const myArray1 = new Array(10).fill(0);
+arr2.fill(0, 2, 5);
+console.log(myArray1);
+console.log(arr2);
+
+// Splice methods
+// start  delete insert
+//change main array
+
+// delete
+arr2.splice(2, 3);
+console.log(arr2);
+
+// insert
+arr2.splice(2, 0, -2, 66, 22);
+console.log(arr2);
