21617c16d9dfb45c9db47f48f1862ce8a1206bdf
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-19 17:41:17 +0530

    Day 10

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/file2_array1.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/file2_array1.js
@@ -39,3 +39,22 @@ console.log(reduce);
 
 let arr2 = [1, 121, 3, 11, 42, 32, 4, 75];
 console.log(arr2.sort((a, b) => a - b));
+
+// Find method
+// return first value that satisfy given condition
+
+const myArray = ["Hello", "cat", "dog", "lion"];
+let ans = myArray.find((string) => string.length === 3);
+console.log(ans);
+
+// Every method
+// return boolean value
+
+let every = arr2.every((value) => value % 2 === 0);
+console.log(every);
+
+// Some methods
+// return boolean value
+
+let some = arr2.some((value) => value % 2 === 0);
+console.log(some);
