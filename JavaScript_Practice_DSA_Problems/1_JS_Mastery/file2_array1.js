5211b655828807924512fabfee97fea15f926cf5
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-19 17:20:21 +0530

    Day 10

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/file2_array1.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/file2_array1.js
@@ -15,3 +15,27 @@ let map = arr.map((value, index) => {
 });
 console.log(map); // return array not modify actual array
 console.log(arr);
+
+// Filter method
+
+let filter = arr.filter((value, index) => {
+  return value % 2 === 0;
+});
+console.log(filter);
+
+// Reduce method
+// accumulator variable store sum
+
+// can pass initial value as argument to accumulator
+// arr.reduce(()=>{},0) => 0 accumulator value
+
+let reduce = arr.reduce((accumulator, index) => {
+  return accumulator + index;
+});
+console.log(reduce);
+
+// Sort method
+// work on ascii value
+
+let arr2 = [1, 121, 3, 11, 42, 32, 4, 75];
+console.log(arr2.sort((a, b) => a - b));
