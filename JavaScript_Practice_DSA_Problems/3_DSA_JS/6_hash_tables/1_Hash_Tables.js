733b47c1d8a498279caba94e1189ba539c4ba92c
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-26 23:03:13 +0530

    Day 16

--- JavaScript_Practice_DSA_Problems/3_DSA_JS/6_hash_tables/1_Hash_Tables.js
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/6_hash_tables/1_Hash_Tables.js
@@ -5,13 +5,27 @@ class HashTable {
   _hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) % this.dataMap.length;
+      hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
     }
     return hash;
+  }
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.dataMap[index]) this.dataMap[index] = [];
+
+    this.dataMap[index].push([key, value]);
   }
 }
 
 // =========== Creating a hash table ===============
 
-const hashTable = new HashTable();
+const hashTable = new HashTable(7);
+console.log(hashTable);
+
+// Set method
+
+hashTable.set("name", "Harshit");
+console.log(hashTable);
+
+hashTable.set("age", 23);
 console.log(hashTable);
