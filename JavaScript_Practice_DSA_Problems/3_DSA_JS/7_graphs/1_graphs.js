0af47a8c78c6cac7ff6eb5e6e0d069d1a1134575
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-28 16:53:35 +0530

    Day 18

--- JavaScript_Practice_DSA_Problems/3_DSA_JS/7_graphs/1_graphs.js
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/7_graphs/1_graphs.js
@@ -30,6 +30,27 @@ class Graph {
     }
     return false;
   }
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
+      return true;
+    }
+    return false;
+  }
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return undefined;
+    while (this.adjacencyList[vertex].length) {
+      let temp = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, temp);
+    }
+    delete this.adjacencyList[vertex];
+    return this;
+  }
 }
 
 // ============== Creating a graph =================
@@ -37,6 +58,7 @@ class Graph {
 let myGraph = new Graph();
 console.log(myGraph.addVertex("A"));
 console.log(myGraph.addVertex("B"));
+console.log(myGraph.addVertex("C"));
 console.log(myGraph.addVertex("A"));
 console.log(myGraph);
 
@@ -44,5 +66,31 @@ console.log(myGraph);
 
 console.log(myGraph.addEdge("A", "B"));
 console.log(myGraph.addEdge("A", "B"));
+console.log(myGraph.addEdge("C", "B"));
 console.log(myGraph.addEdge("A", "A"));
 console.log(myGraph);
+
+// Remove an edge
+
+console.log(myGraph.removeEdge("B", "C"));
+console.log(myGraph);
+
+// Remove a vertex
+
+let myGraph2 = new Graph();
+console.log(myGraph2.addVertex("A"));
+console.log(myGraph2.addVertex("B"));
+console.log(myGraph2.addVertex("C"));
+console.log(myGraph2.addVertex("D"));
+console.log(myGraph2);
+
+console.log(myGraph2.addEdge("A", "B"));
+console.log(myGraph2.addEdge("A", "C"));
+console.log(myGraph2.addEdge("A", "D"));
+console.log(myGraph2.addEdge("D", "B"));
+console.log(myGraph2.addEdge("D", "C"));
+console.log(myGraph2);
+
+console.log(myGraph2.removeVertex("D"));
+console.log(myGraph2.removeVertex("E")); // edge case
+// console.log(myGraph2);
