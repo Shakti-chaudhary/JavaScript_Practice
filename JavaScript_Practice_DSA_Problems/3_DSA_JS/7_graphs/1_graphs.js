4aead9b6a3357d5dab516f5c07ffd0605797b7e1
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-28 15:41:53 +0530

    Day 18

--- JavaScript_Practice_DSA_Problems/3_DSA_JS/7_graphs/1_graphs.js
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/7_graphs/1_graphs.js
@@ -4,3 +4,21 @@
 //   Directional edge {connected to one vertex}
 //   Adjacency matrix
 //   Adjacency list
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+      return true;
+    }
+    return false;
+  }
+}
+
+let myGraph = new Graph();
+console.log(myGraph.addVertex("A"));
+console.log(myGraph.addVertex("A"));
+console.log(myGraph);
