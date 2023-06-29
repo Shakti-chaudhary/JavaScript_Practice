6f536fa1bc3b380a0a593777c5646b1fba752ce7
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-29 16:09:22 +0530

    Day 19

---
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/9_sorting_algorithms/2_selection_sort.js
@@ -0,0 +1,16 @@
+//  ============== Selection Sort ==============
+
+function selectionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [array[i], array[minIndex]] = [array[minIndex], array[i]];
+  }
+  return array;
+}
+
+console.log(selectionSort([11, 2, 2, 63, 14, 5]));
