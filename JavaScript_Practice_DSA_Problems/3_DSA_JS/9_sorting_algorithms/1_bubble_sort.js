b98c80c6e0de820a8e0a6da145164ada8b7228cd
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-29 14:24:58 +0530

    Day 19

---
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/9_sorting_algorithms/1_bubble_sort.js
@@ -0,0 +1,17 @@
+//  ============= Bubble Sort =============
+
+function bubbleSort(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        // [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([4, 55, 1, 8, 3, 11]));
