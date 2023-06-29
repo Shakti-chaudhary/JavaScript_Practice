ddd9955f7ee50edf80b40c34531d74d8bc980903
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-29 22:00:07 +0530

    Day 19

---
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/9_sorting_algorithms/5_quick_sort.js
@@ -0,0 +1,29 @@
+// =============== Quick sort ====================
+
+function swap(array, firstIndex, secondIndex) {
+  [array[firstIndex], array[secondIndex]] = [
+    array[secondIndex],
+    array[firstIndex],
+  ];
+}
+function pivot(array, pivotIndex = 0, endIndex = array.length - 1) {
+  let swapIndex = pivotIndex;
+  for (let i = pivotIndex; i <= endIndex; i++) {
+    if (array[i] < array[pivotIndex]) {
+      swapIndex++;
+      swap(array, swapIndex, i);
+    }
+  }
+  swap(array, pivotIndex, swapIndex);
+  return swapIndex;
+}
+function quickSort(array, left = 0, right = array.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(array, left, right);
+    quickSort(array, left, pivotIndex - 1);
+    quickSort(array, pivotIndex + 1, right);
+  }
+  return array;
+}
+
+console.log(quickSort([21, 2, 2, 33, 33, 14, 5, 69, 17, 28, 88, 39, 10]));
