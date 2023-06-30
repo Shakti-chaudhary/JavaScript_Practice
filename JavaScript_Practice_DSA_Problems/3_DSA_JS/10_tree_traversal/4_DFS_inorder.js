1f81406004300d2a9219cba194e096169aefba98
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-30 12:12:58 +0530

    Day 20

---
+++ JavaScript_Practice_DSA_Problems/3_DSA_JS/10_tree_traversal/4_DFS_inorder.js
@@ -0,0 +1,77 @@
+//  ============ DFS InOrder Traversal ===========
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (newNode.value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    while (current) {
+      if (value > current.value) {
+        current = current.right;
+      } else if (value < current.value) {
+        current = current.left;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+  DFSInOrder() {
+    let results = [];
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      results.push(currentNode.value);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
+}
+
+//   ========== Creating a tree =================
+
+let myTree = new BST();
+myTree.insert(47);
+myTree.insert(21);
+myTree.insert(76);
+myTree.insert(18);
+myTree.insert(27);
+myTree.insert(52);
+myTree.insert(82);
+
+console.log(myTree);
+console.log(myTree.DFSInOrder());
