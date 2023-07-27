6fdd6c74a7a6ff9438a04d6e7f01e11336cd92d2
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-07-27 22:10:10 +0530

    Day 35

---
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/file7_JS.js
@@ -0,0 +1,12 @@
+// ================ How javascript work ==========================
+// Compilation
+// Code execution
+
+// why compilation
+
+// how javascript code executes
+
+// what is global execution context ?
+// what is local execution context ?
+
+// ====================== Hoisting ==============================
