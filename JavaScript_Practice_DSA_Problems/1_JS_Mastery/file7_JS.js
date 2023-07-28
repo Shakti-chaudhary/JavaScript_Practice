675c7290c2cd07abf8838c6f17e99e599beaede6
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-07-28 21:33:37 +0530

    Day 35

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/file7_JS.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/file7_JS.js
@@ -23,10 +23,63 @@
 
 // TDZ { Temporal dead zone }
 
-// ====================== Hoisting ==============================
+// =================== Hoisting ========================
 // move variables to top of there scope
 
 // ==== Scope chain and lexical environment
 // function has access to outer scope
 
-// ============ Closures ========================
+// ================== Closures ========================
+
+// function can return function
+
+function printFullName(firstName, lastName) {
+  function printName() {
+    console.log(firstName, " ", lastName);
+  }
+  return printName;
+}
+const ans = printFullName("Rahul", "Singh");
+ans();
+
+// Example 1.
+
+function hello(x) {
+  const a = "varA";
+  const b = "varB";
+  return function () {
+    console.log(a, b, x);
+  };
+}
+
+const ans1 = hello("arg");
+ans1();
+
+// Example 2.
+
+function myFunction(power) {
+  return function (number) {
+    return number ** power;
+  };
+}
+
+const ans2 = myFunction(3);
+console.log(ans2(2)); // 8
+
+// Example 3.
+
+function func() {
+  let counter = 0;
+  return function () {
+    if (counter < 1) {
+      console.log("Hi You Called Me");
+    } else {
+      console.log("Mai already ek bar call ho chuka hoon!");
+    }
+  };
+}
+
+const myFunc = func();
+myFunc();
+myFunc();
+myFunc();
