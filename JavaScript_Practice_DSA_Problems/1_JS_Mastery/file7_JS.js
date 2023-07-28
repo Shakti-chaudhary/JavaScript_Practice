b0b9ee3fddb9554f61e573e0ea59e586bbde6182
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-07-28 20:13:29 +0530

    Day 35

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/file7_JS.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/file7_JS.js
@@ -6,7 +6,27 @@
 
 // how javascript code executes
 
+// 1. compilation
+
+//  compilation consist of two part
+// 1. checks for errors.
+// 2. Scopes checking.
+
+// 2. Code execution -> In order to execute js code js creates execution context
+
+// 1. globel execution context
+// 1. Creation phase -> the global memory allocation
+// 2. code execution ( code run line by line ).
+
 // what is global execution context ?
-// what is local execution context ?
+// what is local {Function} execution context ?
+
+// TDZ { Temporal dead zone }
 
 // ====================== Hoisting ==============================
+// move variables to top of there scope
+
+// ==== Scope chain and lexical environment
+// function has access to outer scope
+
+// ============ Closures ========================
