81ea500210ccaeee7d10fd8c5a0fa54a7a77c58b
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-08-09 20:43:18 +0530

    Day 40

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/Dom_Tutorial_file8/file8_DOM.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/Dom_Tutorial_file8/file8_DOM.js
@@ -1,1 +1,27 @@
 // ============== Dom learn ======================
+// DOM
+// document object model
+// overview
+// how to use
+// deep study
+
+// select element usig get element by id
+
+console.log("hello word");
+console.log(document.getElementById("main-heading"));
+
+// select element using query selector
+
+console.log(document.querySelector("#main-heading"), " Query selector!");
+
+const header = document.querySelector(".header");
+console.log(header);
+
+const navItem = document.querySelectorAll(".nav-item");
+console.log(navItem);
+
+// change text
+// textContent and innerText
+
+const mainheading1 = document.getElementById("main-heading");
+console.log(mainheading1.textContent);
