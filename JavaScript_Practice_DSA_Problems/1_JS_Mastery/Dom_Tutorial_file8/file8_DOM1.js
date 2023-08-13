c89054ee34492a7190110e744e6f594a62fd1b81
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-08-13 21:42:44 +0530

    Day 41

---
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/Dom_Tutorial_file8/file8_DOM1.js
@@ -0,0 +1,5 @@
+// ================= classList =================
+const sectionTodo = document.querySelector(".section-todo");
+console.log(sectionTodo);
+// sectionTodo.classList.add("bg-dark");
+// sectionTodo.classList.remove("container");
