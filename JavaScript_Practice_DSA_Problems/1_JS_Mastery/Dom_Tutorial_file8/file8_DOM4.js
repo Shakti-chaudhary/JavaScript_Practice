a920002156c451609d8ded11beddde77caa59ac5
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-08-20 00:27:17 +0530

    Day 43

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/Dom_Tutorial_file8/file8_DOM4.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/Dom_Tutorial_file8/file8_DOM4.js
@@ -1,4 +1,42 @@
 // =================== event bubbling ===================
+
+const grandparent = document.querySelector(".grandparent");
+const parent = document.querySelector(".parent");
+const child = document.querySelector(".child");
+
+child.addEventListener("click", () => {
+  console.log("you clicked on child");
+});
+
+parent.addEventListener("click", () => {
+  console.log("you clicked on parent");
+});
+
+grandparent.addEventListener("click", () => {
+  console.log("you clicked on grandparent");
+});
+
 // =================== event capturing ===================
+
+parent.addEventListener(
+  "click",
+  () => {
+    console.log("capture on parent");
+  },
+  true
+);
+
+grandparent.addEventListener(
+  "click",
+  () => {
+    console.log("capture on grandparent");
+  },
+  true
+);
+
 // =================== event delegation ===================
-console.log("events");
+
+const grandparent1 = document.querySelector(".grandparent");
+grandparent1.addEventListener("click", (e) => {
+  console.log(e.target);
+});
