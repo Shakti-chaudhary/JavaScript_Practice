c0d653e17e7d0757ae8ff96fefd7132da961e083
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-08-23 22:38:11 +0530

    Day 44

--- JavaScript_Practice_DSA_Problems/1_JS_Mastery/Async_Tutorial_file9/file9_async.js
+++ JavaScript_Practice_DSA_Problems/1_JS_Mastery/Async_Tutorial_file9/file9_async.js
@@ -1,12 +1,52 @@
 // synchronous programming vs asynchronous programming
-
 // synchronous programming
 
-// setTimeout
+// ============== setTimeout ===============
 
 console.log("script start");
 function hello() {
   console.log("Inside setTimeout");
 }
-setTimeout(hello, 1000);
+
+const id = setTimeout(hello, 1000); // take time in miliSecond and return an id
+console.log("setTimeout id is : ", id);
+console.log("clearing time out ");
+clearTimeout(id); // clear setTimeout with id
+
 console.log("script end");
+
+// =============== setInterval ===============
+
+console.log("setInterval script start");
+
+const id1 = setInterval(() => {
+  console.log(Math.floor(Math.random() * 100));
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(id1);
+  console.log("setInterval clear");
+}, 5000); // clear setInterval in interval of 5 second
+
+console.log("setInterval script end");
+
+// =============== understand callback ===========
+
+function getTwoNumberAndAdd(number1, number2, onSuccess, onFailure) {
+  if (typeof number1 === "number" && typeof number2 === "number") {
+    onSuccess(number1, number2);
+  } else {
+    onFailure();
+  }
+}
+
+getTwoNumberAndAdd(
+  4,
+  4,
+  (num1, num2) => {
+    console.log(num1 + num2, " <==  Added value is here ");
+  },
+  () => {
+    console.log("please number only ");
+  }
+);
